ac8d0a20a93796ae7c9556de32b9f5ee96c0c7fb
admin <admin>
2022-06-09 18:09:03 +0200

    edit files

--- lib/python/preprocessing/weather.py
+++ lib/python/preprocessing/weather.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def na_handling(df, columns):
+def TM_handling(df, columns):
     for c in columns:
         df[c].replace(["M", "T"], [0, 0.0001], inplace=True)
     return df
