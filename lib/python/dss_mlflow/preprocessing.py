d0d8afad1dd6919618de3223cee49233850f3a4c
admin <admin>
2022-07-20 17:46:10 +0800

    edit file

--- lib/python/dss_mlflow/preprocessing.py
+++ lib/python/dss_mlflow/preprocessing.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
+month_dic = {
+    'apr': 5,
+    'aug': 8,
+    'dec': 12,
+    'feb': 2,
+    'jan': 1,
+    'jul': 7,
+    'jun': 6,
+    'mar': 3,
+    'may': 5,
+    'nov': 11,
+    'oct': 10,
+    'sep': 9}
+
 def clean_df(df):
     y = df['y']
     X = df.drop('y', axis=1)
